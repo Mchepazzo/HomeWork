c1902335569aac4af021469e5a9b4fd631032b26
Mchepazzo <44810351+Mchepazzo@users.noreply.github.com>
2019-05-22 15:57:28 +0500

    task15-1

---
+++ ru/mardaganiev/task15/WorkWithFiles.java
@@ -0,0 +1,59 @@
+package ru.mardaganiev.task15;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class WorkWithFiles {
+    public static void main(String[] args) {
+        System.out.println("==Создание файла==");
+
+        File file = new File("C:/Users/DNS/Downloads/Учёба Java/Занятия/Task15/temp.txt");
+
+        try {
+            System.out.println("Файл " + file.getName() + " был создан: " + file.createNewFile());
+        } catch (IOException e) {
+            System.out.println("Не удалось создать файл: " + file.getName() + " : " + e.getMessage());
+        }
+
+        System.out.println("Файл " + file.getName() + " существует: " + file.exists());
+
+        System.out.println(" ");
+        System.out.println("==Копирование файла==");
+
+        Path originalPath = Paths.get("C:/Users/DNS/Downloads/Учёба Java/Занятия/Task15/temp.txt");
+        Path pathTarget = Paths.get("C:/Users/DNS/Downloads/Учёба Java/Занятия/Task15/CopyOfTemp.txt");
+
+        try {
+
+            Files.copy(originalPath, pathTarget, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Файл был скопирован");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(" ");
+        System.out.println("==Переименовывание файла==");
+
+        System.out.println("Файл " + file.getName() + " был переименован: " + file.renameTo(file));
+        file.renameTo(new File("C:/Users/DNS/Downloads/Учёба Java/Занятия/Task15/temp2.txt"));
+
+        File newFile = new File("temp2.txt");
+        System.out.println("Файл " + newFile.getName() + " существует: " + newFile.exists());
+
+        System.out.println(" ");
+        System.out.println("==Удаление файла==");
+
+        System.out.println("Файл " + newFile.getName() + " был удалён: " + ": " + newFile.delete());
+
+    }
+
+    private static void file(Path copy_from_1, Path copy_to_1) {
+
+
+    }
+}
