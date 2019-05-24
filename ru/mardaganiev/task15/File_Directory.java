6b6d79654a49106b1ac44ab351df2ff3d2f4a73a
Mchepazzo <44810351+Mchepazzo@users.noreply.github.com>
2019-05-24 13:25:36 +0500

    Add files via upload

---
+++ ru/mardaganiev/task15/File_Directory.java
@@ -0,0 +1,36 @@
+package ru.mardaganiev.task15;
+
+import java.io.*;
+import java.util.*;
+
+
+class File_Directory {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите адрес исследуемого каталога: ");
+        String Path = scanner.nextLine();
+        printTreeFiles(Path);
+    }
+
+    public static void printTreeFiles(String Path) {
+        File Directory = new File(Path);
+
+        if (Directory.exists()) {
+            getContent(Directory, 0);
+        } else System.out.println("Введён неверный адрес каталога...");
+    }
+
+    public static void getContent(File Directory, int Indents) {
+        for (int i = 0; i < Indents; i++) System.out.print("\t");
+
+        if (Directory.isFile()) System.out.println("В каталоге: " + Directory.getParent() + " находится" +
+                " файл: " + Directory.getName());
+        else {
+            System.out.println(Directory.getName());
+            File[] SubDirectory = Directory.listFiles();
+            for (File SubWay : SubDirectory)
+                getContent(SubWay, Indents + 1);
+        }
+    }
+}
